c8b662c9751d3a5227e9c0ffbe0bffddf3d7613b
Haylee1787 <142622305+Haylee1787@users.noreply.github.com>
2024-04-11 13:12:37 -0700

    removed unused libraries. serialized public properties. SpawnManager.cs

--- Prototype4/Assets/Scripts/SpawnManager.cs
+++ Prototype4/Assets/Scripts/SpawnManager.cs
@@ -1,14 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject enemyPrefab;
-    public GameObject powerupPrefab;
-    public float spawnRange = 9.0f;
-    public int waveNumber = 1;
-    public int enemyCount;
+    [SerializedFeild] private GameObject enemyPrefab;
+    [SerializedFeild] private GameObject powerupPrefab;
+    [SerializedFeild] private float spawnRange = 9.0f;
+    [SerializedFeild] private int waveNumber = 1;
+    [SerializedFeild] private int enemyCount;
     // Start is called before the first frame update
     void Start()
     {
